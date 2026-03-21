ba048685f0e50308c611ab2c9a205a45a33d5ba0
Lucas-Urbanski <lcurbanski8@gmail.com>
2026-03-21 15:35:08 -0600

    StackADT documentation

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -1,25 +1,56 @@
 package utilities;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * QueueADT.java
  * 
- * @author Xander Mulligan and Alessandro Pirela leal
- * @version 1.0
+ * @author Xander Mulligan & Alessandro Pirela
+ * @version 1.1
  * 
  * Class Definition: This interface is designed to be implemented by a queue class.
  * All methods must be implemented by the class.
  * 
- * @param <E> The type of elements that the queue stores. 
+ * @param <E> The type of elements that the queue stores.
  */
 public interface QueueADT<E> {
-	
+
+	/**
+	 * Mutator method to create a queue of a certain size.
+	 * 
+	 * Precondition: A Queue does not exist.
+	 * 
+	 * Postcondition: A Queue is created with the specified size.
+	 * 
+	 * @param size: The size of the queue that is being created.
+	 */
 	public void createQueue(int size);
 
+	/**
+	 * Mutator method to add an element to the back of the queue.
+	 * 
+	 * Precondition: A Queue exists and the item is not null.
+	 * 
+	 * Postcondition: The item is added to the back of the queue.
+	 * 
+	 * @param item: The item that is being added to the back of the queue.
+	 */
 	public void enqueue(E item);
-	
-	public E peek();
-	
+
+	/**
+	 * Accessor method to return the first element in the queue.
+	 * 
+	 * Precondition: A Queue exists and is not empty.
+	 * 
+	 * Postcondition: The first element of the queue is returned.
+	 * 
+	 * @return the item that is at the front of the queue.
+	 * 
+	 * @throws NoSuchElementException if the queue is empty.
+	 */
+	public E peek() throws NoSuchElementException;
+
 	/**
 	 * Mutator method to remove the first element in the queue.
 	 * 
@@ -28,11 +59,13 @@ public interface QueueADT<E> {
 	 * Postcondition: The first element of the queue is removed.
 	 * 
 	 * @return the item that was removed.
+	 * 
+	 * @throws NoSuchElementException if the queue is empty.
 	 */
-	public E dequeue();
-	
+	public E dequeue() throws NoSuchElementException;
+
 	/**
-	 * Transformer method to check if two queues contain the 
+	 * Transformer method to check if two queues contain the
 	 * same items in the same order.
 	 * 
 	 * Precondition: Two Queues exist.
@@ -44,7 +77,7 @@ public interface QueueADT<E> {
 	 * @return true if the queues are the same else false.
 	 */
 	public boolean equals(QueueADT<E> otherQueue);
-	
+
 	/**
 	 * Transformer method that returns the queue as a iterator.
 	 * 
@@ -55,13 +88,13 @@ public interface QueueADT<E> {
 	 * @return the iterator representation of the queue.
 	 */
 	public Iterator<E> iterator();
-	
+
 	public Object[] toArray();
-	
+
 	public E[] toArray(E[] copy);
-	
+
 	public boolean isFull();
-	
+
 	/**
 	 * Accessor method to return the queue size.
 	 * 
@@ -72,7 +105,7 @@ public interface QueueADT<E> {
 	 * @return a integer that is representation of the size of the queue.
 	 */
 	public int size();
-	
+
 	/**
 	 * Transformer method to check if the queue is empty
 	 * 
@@ -84,7 +117,7 @@ public interface QueueADT<E> {
 	 * @return true if the queue is empty else false.
 	 */
 	public boolean isEmpty();
-	
+
 	/**
 	 * Mutator method to remove all of the elements in the queue.
 	 * 
@@ -93,5 +126,4 @@ public interface QueueADT<E> {
 	 * Postcondition: The all of the elements of the queue is removed.
 	 */
 	public void dequeueAll();
-	
 }
